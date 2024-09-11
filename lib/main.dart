f8b9a5fee1de34c59b813c9b314772bfe7abf7ad
Giovanni Dias <giovannin.dias@unigex.com.br>
2024-09-11 15:56:30 -0300

    feat: inserindo botoes

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,39 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(PerguntaApp());
-}
+main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
-  
+  const PerguntaApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text('Olá Flutter'),
-    );
+    final perguntas = [
+      'Qual é a sua cor favorita',
+      'Qual é o seu animal favorito',
+    ];
+
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text(perguntas[0]),
+      ),
+      body: Column(
+        children: [
+          Text(perguntas[0]),
+          ElevatedButton(
+            onPressed: () => print('Resposta 1!'),
+            child: const Text('Resposta 1'),
+          ),
+          ElevatedButton(
+            onPressed: () => print('Resposta 2!'),
+            child: const Text('Resposta 2'),
+          ),
+          ElevatedButton(
+            onPressed: () => print('Resposta 3!'),
+            child: const Text('Resposta 3'),
+          ),
+        ],
+      ),
+    ));
   }
 }
