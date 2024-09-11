f14a6b99b660d4c0acae3f406077aa885537189b
Giovanni Dias <giovannin.dias@unigex.com.br>
2024-09-11 10:27:43 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Text('Olá Flutter'),
+    );
+  }
+}
